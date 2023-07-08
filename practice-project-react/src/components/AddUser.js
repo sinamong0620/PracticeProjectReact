99a7d21b4f5e343d77f3cf4c67c282978d64a85b
sinamong0620 <sinaskim60@gmail.com>
2023-07-08 23:43:52 +0900

    AddUser form input event
    AddUser 컴포넌트에서 Input에서 이벤트를 발생시켜서 state에 저장함

--- practice-project-react/src/components/AddUser.js
+++ practice-project-react/src/components/AddUser.js
@@ -1,21 +1,42 @@
+import { useState } from "react";
 import styles from "./AddUser.module.css";
 
-const AddUser = () => {
+const AddUser = (props) => {
+  const [userName, setUserName] = useState(""); //사용자 이름
+  const [userAge, setUserAge] = useState(""); //사용자 나이
+
+  const AddUserName = (e) => {
+    setUserName(e.target.value);
+  };
+  const AddUserAge = (e) => {
+    setUserAge(e.target.value);
+  };
+  const SubmitUserForm = (e) => {
+    e.preventDefault();
+
+    const expenseData = {
+      name: userName,
+      age: userAge,
+    };
+
+    props.onSaveExpenseData(expenseData);
+  };
   return (
     <>
       <div className={styles.adduserform}>
-        <form>
+        <form onSubmit={SubmitUserForm}>
           <label>Username</label>
           <div>
-            <input type="text" />
+            <input type="text" onChange={AddUserName} />
           </div>
 
           <label>Age(Years)</label>
           <div>
-            <input type="text" />
+            <input type="text" onChange={AddUserAge} />
           </div>
+
+          <button>Add User</button>
         </form>
-        <button>Add User</button>
       </div>
     </>
   );
